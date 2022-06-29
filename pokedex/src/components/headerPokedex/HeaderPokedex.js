7dee6643a16e88c2574b79dc64b37905dd83812c
Daniel Nascimento Firmiano <daniel_2dfla@hotmail.com>
2022-06-28 23:10:29 -0300

    finalizando rotas

--- pokedex/src/components/headerPokedex/HeaderPokedex.js
+++ pokedex/src/components/headerPokedex/HeaderPokedex.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {useNavigate} from "react-router-dom";
+import { goToHomePage } from '../../coordinator/Coordinator';
 
 const HeaderPokedex = () => {
+  const navigate = useNavigate()
     
   return (
     <div>
-        <button>Voltar para lista de pokemons</button>
+        <button onClick={()=> goToHomePage(navigate)} >Voltar para lista de pokemons</button>
         <h1>Pokedex!!</h1>
     </div>
   )
