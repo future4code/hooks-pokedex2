79768e3e55b54118fbea5250b077345401f2ab7b
Perkles <adrianegonaves@gmail.com>
2022-07-05 15:30:22 -0300

    Renderizando as imagens e nomes do pokemon

--- pokedex/src/pages/home/Home.js
+++ pokedex/src/pages/home/Home.js
@@ -1,66 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import HeaderHome from '../../components/headerHome/HeaderHome';
-import { ButtonsCard, Card } from '../../styles';
-import styled from 'styled-components';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import HeaderHome from "../../components/headerHome/HeaderHome";
+import { ButtonsCard, Card } from "../../styles";
+import styled from "styled-components";
+import CardPokemon from "../../components/cardPokemon/CardPokemon";
 
 const DivImg = styled.div`
-background-color: blue;
-height: 250px;
-width: 100%;
-`
+  background-color: blue;
+  height: 250px;
+  width: 100%;
+`;
 
 const DivCard = styled.div`
-display: flex ;
-flex-wrap : wrap
-`
+  display: flex;
+  flex-wrap: wrap;
+`;
 const Home = () => {
-  const [pokemon, setPokemon] = useState ([])
+  const [pokemon, setPokemon] = useState([]);
 
-
-  useEffect (() => {
-    handlePokemons()
-  }, [])
+  useEffect(() => {
+    handlePokemons();
+  }, []);
 
   const handlePokemons = () => {
     axios
-    .get('https://pokeapi.co/api/v2/pokemon/')
-    .then((res) => {
-      setPokemon(res.data.results)
-      // console.log(res.data.results)
-     
+      .get("https://pokeapi.co/api/v2/pokemon/")
+      .then((res) => {
+        setPokemon(res.data.results);
+        // console.log(res.data.results)
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
 
-    })
-    .catch((erro) => {
-      console.log(erro)
-    })
-  }
-  
   const listPokemons = pokemon.map((pokemon) => {
     return (
-    <div>
-      <Card>
+      <div>
+        <Card>
           <DivImg>{pokemon.name}</DivImg>
           <div>
-
-          <ButtonsCard>adicionar a pokedex</ButtonsCard>
-          <ButtonsCard>detalhes</ButtonsCard>
+            <ButtonsCard>adicionar a pokedex</ButtonsCard>
+            <ButtonsCard>detalhes</ButtonsCard>
           </div>
         </Card>
-    </div>
-    )
-  })
+      </div>
+    );
+  });
   return (
     <div>
-        <HeaderHome/>
-        <DivCard>
-          {listPokemons}
-        </DivCard>
-        <div>Home</div>
-        
+  
+      <HeaderHome />
+      <DivCard>
+      
+      </DivCard>
+      <CardPokemon />
+      <div>Home</div>
+      <CardPokemon />
+  
+     
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
